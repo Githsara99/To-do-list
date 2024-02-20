0a7dfea6e9936027afc5e396724fda72a4e10b46
GithsaraS <Ishancharun@icloud.com>
2024-02-20 17:56:42 +0530

    add mongo

--- task-manager/api/app.js
+++ task-manager/api/app.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require( 'body-parser' );
+
+const {mongoose} = require('./db/mongoose')
+
+const {List} = require("./models/list.model")
+const {Task}  = require("./models/task.model");
+
+app.use(bodyParser.json());
+
 app.get('/lists', (req,res) => {
-   
+    List.find({}).then((lists) => {
+        res.send(lists);
+    }).catch((e) =>{
+        res.send(e);
+    });   
 })
 
 app.post('/lists', (req, res) => {
+    let title = req.body.title;
 
-})
+    let newList = new List({
+        title
+    });
+    newList.save().then((listDoc)=>{
+        res.send(listDoc);
+        }).catch((err)=>console.log(err))
+
+    });
 
 app.patch('/lists/:id',(req,res)=>{
-
+    List.findOneAndUpdate({_id: req.params.id},{
+        $set:req.body}).then(()=>{
+        res.sendStatus(200);
+})
 })
 
 app.delete('/lists/:id',(req,res)=> {
